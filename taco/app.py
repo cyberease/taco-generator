4ad43d1e6c3e08b41e2e6fbf81d3a1cad25cb094
Kevin Nelson <42252270+cyberease@users.noreply.github.com>
2021-08-09 12:19:56 -0500

    Initial Commit

---
+++ taco/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/your-taco', methods=['GET', 'POST'])
+def your_taco():
+    if request.method == 'POST':
+        return render_template('your-taco.html', no_ingredients=request.form['no_ingredients'])
+    else:
+        return redirect(url_for('home'))
