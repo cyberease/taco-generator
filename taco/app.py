9a290f602492c2029c450d2dce14e5b99a8033df
Kevin Nelson <kevin.nelson@cyberease.net>
2021-08-09 16:51:23 -0500

    added test script simple.py

--- taco/app.py
+++ taco/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-import json
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -14,6 +14,12 @@ def your_taco():
         proteins = data['proteins']
         veggies = data['veggies']
         dairy = data['dairy']
+        if proteins + veggies + dairy > 0:
+            if proteins > 1:
+                proteinList = pd.read_csv("proteins.csv")
+                i=0
+                for i in range (0..proteins):
+                    
         return render_template('your-taco.html', no_proteins=proteins, no_veggies=veggies, no_dairy_items=dairy)
     else:
         return redirect(url_for('home'))
